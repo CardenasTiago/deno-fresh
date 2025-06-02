256b3b05f3190e4f60c3d886ea5ddd15982dac88
CardenasTiago <cardenas.tiago.r@gmail.com>
2025-06-02 20:52:48 -0300

    se agregaron dependencias y cambio de bd

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $api_register from "./routes/api/register.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $login from "./routes/login.tsx";
@@ -16,6 +17,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/api/register.ts": $api_register,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/login.tsx": $login,
--- lib/config/database.ts
+++ lib/config/database.ts
@@ -5,7 +5,7 @@ const POOL_CONNECTIONS = 3;
 const dbPool = new Pool({
   user: "postgres",
   password: "password",
-  database: "deno_db",
+  database: "studentapp",
   hostname: "localhost",
   port: 5432,
 }, POOL_CONNECTIONS, true);
